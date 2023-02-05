9a545cc562222989dc2ff440a787dbaee20da27c
John Doe <johndoe@example.com>
2023-02-05 16:13:03 -0500

    Primer Comit

---
+++ myFirstBackend/Program.cs
@@ -0,0 +1,47 @@
+//0. Usings para trabajar con EntityFramework
+using Microsoft.EntityFrameworkCore;
+using myFirstBackend.DataAcces;
+
+var builder = WebApplication.CreateBuilder(args);
+
+/*1. Conexion con la Base de Datos
+ *2.Dependencias:
+ *  -Microsoft.EntityFrameworkCore.SqlServer
+ *  -Microsoft.EntityFrameworkCore.Tools, ejecutar desde terminal
+ *  -Microsoft.VisualStudio.Web.CodeGeneration.Design, trae plantillas crud
+ *  -Microsoft.VisualStudio.Web.CodeGeneration.Core,
+ *  --Extension EF Core Power Tools, permite hacer un diagrama del DBContext
+ * */
+//3.Conexxion con SQL Server
+const string name = "DefaultConnection";
+
+var connectionString=builder.Configuration.GetConnectionString(name);
+
+//4. Añadir contexto-Usind Dta acces
+builder.Services.AddDbContext<UniversityDBContext>(options => options.UseSqlServer(connectionString));
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
